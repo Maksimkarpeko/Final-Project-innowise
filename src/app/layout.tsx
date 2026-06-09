9494fc3d3ece99872e2b7bdeb738b2ed9a01a080
Maksim <maksim_karpe2ko@mail.ru>
2026-06-09 10:40:49 +0300

    feat[auth]: added next-config redirect to the login page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConfigProvider } from "antd";
-import { WrapperApollo } from "./apollo-client";
+import { WrapperApollo } from "./apollo/apollo-client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import { PATH } from "@/src/shared/config/path.config";
-
-export default function Home() {
-    redirect(PATH.AUTH.LOGIN);
-}
